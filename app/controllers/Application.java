a3328027e1aadbd967fcdb3d208c4222a9280169
Riza Baltazar <rizbalt@gmail.com>
2015-02-22 10:50:44 -0500

    front end

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,35 +1,50 @@
 package controllers;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("hello."));
+        return ok(index.render(""));
     }
     
     public static Result allevents() {
-        return ok(allevents.render("hello."));
+        return ok(allevents.render(""));
     }
     
     public static Result myevents() {
-        return ok(myevents.render("hello."));
+        return ok(myevents.render(""));
     }
     
     public static Result rinkmaps() {
-        return ok(rinkmaps.render("hello."));
+        return ok(rinkmaps.render(""));
     }
     public static Result parkmaps() {
-        return ok(parkmaps.render("hello."));
+        return ok(parkmaps.render(""));
     }
     public static Result bikemaps() {
-        return ok(bikemaps.render("hello."));
+        return ok(bikemaps.render(""));
     }
     public static Result allmaps() {
-        return ok(allmaps.render("hello."));
+        return ok(allmaps.render(""));
+    }
+    
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
     }
     
 }
