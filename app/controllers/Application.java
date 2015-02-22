860704f6fd3b5ec9421cc6adc02517c08b994883
Riza Baltazar <rizbalt@gmail.com>
2015-02-22 10:59:49 -0500

    test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -37,14 +37,4 @@ public class Application extends Controller {
         return ok(allmaps.render(""));
     }
     
-    private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
-    }
-    
 }
